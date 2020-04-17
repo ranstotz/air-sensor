06251fd8eaa8ed001d5fce08fe92d52133ef20ea
Ryan Anstotz <ryananstotz@gmail.com>
2020-04-16 22:48:36 -0400

    modify time to produce nearest-second accuracy and execution.

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import time
 
 import random
 
-def collect_data(sensor_obj):
-    year, month, day, hour, minute = map(int, time.strftime("%Y %m %d %H %M").split())
-
-    print(year, " : ", month, " : ", day, " : ", hour, " : ", minute)
-    for x in range(10):
-        print(random.randint(1,101))
-  
+def collect_air_data(sensor_obj):
+    starttime=time.time()
+    while True:
+        print("tick")
+        #print(random.randint(1,101))
+        year, month, day, hour, minute, sec = map(int, time.strftime("%Y %m %d %H %M %S").split())
+        print(year, " : ", month, " : ", day, " : ", hour, " : ", minute, " : ", sec)
+        time.sleep(1.0 - ((time.time() - starttime) % 1.0))
 
 def main(argv):
-    collect_data("nothing")
+    collect_air_data("nothing")
 
 
 if __name__ == "__main__":
