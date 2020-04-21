c89c70cd8737e14c6a9b15a14fc88309735a2f19
Ryan Anstotz <ryananstotz@gmail.com>
2020-04-20 22:01:47 -0400

    update python config.

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import os
 import sys
 import time
-
+import boto3
 import random
 
-def collect_air_data(sensor_obj):
+
+def main(argv):
+
     starttime=time.time()
     while True:
         print("tick")
         #print(random.randint(1,101))
-        year, month, day, hour, minute, sec = map(int, time.strftime("%Y %m %d %H %M %S").split())
+        year, month, day, hour, minute, sec = \
+            map(int, time.strftime("%Y %m %d %H %M %S").split())
         print(year, " : ", month, " : ", day, " : ", hour, " : ", minute, " : ", sec)
         time.sleep(1.0 - ((time.time() - starttime) % 1.0))
 
-def main(argv):
-    collect_air_data("nothing")
 
 
 if __name__ == "__main__":
